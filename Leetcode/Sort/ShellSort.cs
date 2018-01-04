8a16eb19dcf19fdbb6594e3b5eddc4604ac67808
justboy1987 <175636422@qq.com>
2018-01-04 14:37:35 +0800

    ShellSort

---
+++ Leetcode/Sort/ShellSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Sort
+{
+    class ShellSort
+    {
+        private static void IntArrayShellSort(int[] data, int[] intervals)
+        {
+            int i, j, k, m;
+            int N = data.Length;
+
+            // The intervals for the shell sort must be sorted, ascending
+
+            for (k = intervals.Length - 1; k >= 0; k--)
+            {
+                int interval = intervals[k];
+                for (m = 0; m < interval; m++)
+                {
+                    for (j = m + interval; j < N; j += interval)
+                    {
+                        for (i = j; i >= interval && data[i] < data[i - interval]; i -= interval)
+                        {
+                            exchange(data, i, i - interval);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void IntArrayShellSortNaive(int[] data)
+        {
+            int[] intervals = { 1, 2, 4, 8 };
+            IntArrayShellSort(data, intervals);
+        }
+        private static void exchange(int[] data, int m, int n)
+        {
+            int temporary;
+
+            temporary = data[m];
+            data[n] = temporary;
+        }
+    }
+}
