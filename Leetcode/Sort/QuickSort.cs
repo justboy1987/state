c186b86f1ea6ac2cd57f4ab609fb608014b8111e
justboy1987 <175636422@qq.com>
2018-01-08 07:04:51 +0800

    QuickSortSolution
    QuickSortSolution

---
+++ Leetcode/Sort/QuickSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Sort
+{
+    class QuickSortSolution
+    {
+        public static void QuickSort(int[]a, int s, int e)
+        {
+            if (s < e)
+            {
+                int pos = Partition(a, s, e);
+                QuickSort(a, s, pos -1);
+                QuickSort(a, pos+1, e);
+            }
+        }
+
+        private static int Partition(int[] a, int s, int e)
+        {
+            var pivot = a[s];
+            while (s < e)
+            {
+                while (s < e & a[e] > pivot) e--;
+                a[s] = a[e];
+                while (s < e & a[s] < pivot) s++;
+                a[e] = a[s];
+            }
+            a[s] = pivot;
+            return s;
+        }
+    }
+}
