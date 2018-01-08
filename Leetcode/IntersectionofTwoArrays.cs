c2746874bb3543d1473354aff44da772474a423a
justboy1987 <175636422@qq.com>
2018-01-08 09:17:43 +0800

    IntersectionofTwoArrays

---
+++ Leetcode/IntersectionofTwoArrays.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    class IntersectionofTwoArrays
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            var numbers = new HashSet<int>();
+            foreach (var n in nums1)
+            {
+                numbers.Add(n);
+            }
+            var ret = new HashSet<int>();
+            foreach (var n in nums2)
+            {
+                if (numbers.Contains(n))
+                {
+                    ret.Add(n);
+                }
+            }
+            return ret.ToArray();
+        }
+    }
+}
---
+++ Leetcode/Sort/HeadSort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Sort
+{
+    class HeadSortSLN
+    {
+        private static void BuildHeap(int[]h, int length)
+        {
+            for (int i = (length - 1) / 2; i >= 0; i--)
+            {
+                HeapAdjust(h, i , length);
+            } 
+        }
+
+        public void HeapSort(int[] h, int length)
+        {
+            BuildHeap(h, length);
+            for (int i = length - 1; i > 0; i--)
+            {
+                Swap(h, i, 0);
+                HeapAdjust(h,0,i);
+            }
+        }
+        private static void HeapAdjust(int[]h, int s, int length)
+        {
+            int temp = h[s];
+            int child = 2 * s + 1; // left child
+            while (child < length)
+            {
+                if (child + 1 < length && h[child] < h[child + 1])
+                {
+                    ++child;
+                }
+                if (h[s] < h[child])
+                {
+                    h[s] = h[child];
+                    s = child;
+                    child = 2 * s + 1;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            h[s] = temp;
+        }
+
+        private static void Swap(int[] data, int m, int n)
+        {
+            int temporary;
+
+            temporary = data[m];
+            data[n] = temporary;
+        }
+    }
+}
