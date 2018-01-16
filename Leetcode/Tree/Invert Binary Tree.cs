7efa7e8c43236b2d42d241d709991a521dd53573
justboy1987 <175636422@qq.com>
2018-01-16 12:52:36 +0800

    Invert_Binary_Tree

---
+++ Leetcode/Tree/Invert Binary Tree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Tree
+{
+    class Invert_Binary_Tree
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+            var left = InvertTree(root.right);
+            root.right = InvertTree(root.left);
+            root.left = left;
+            return root;
+        }
+    }
+}
