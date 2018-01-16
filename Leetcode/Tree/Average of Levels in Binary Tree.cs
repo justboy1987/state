5e1c8f2ac2298dc325d4d05a65a9b48d77d02b05
justboy1987 <175636422@qq.com>
2018-01-17 00:40:18 +0800

    Maximum_Binary_Tree

---
+++ Leetcode/Tree/Average of Levels in Binary Tree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Tree
+{
+    class Average_of_Levels_in_Binary_Tree
+    {
+        public IList<double> AverageOfLevels(TreeNode root)
+        {
+
+        }
+    }
+}
---
+++ Leetcode/Tree/Maximum Binary Tree.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Tree
+{
+    class Maximum_Binary_Tree
+    {
+        public TreeNode ConstructMaximumBinaryTree(int[] nums)
+        {
+            return Helper(nums, 0, nums.Length - 1);
+        }
+
+        public TreeNode Helper(int[] nums, int l, int r)
+        {
+            if (l > r)
+            {
+                return null;
+            }
+            var max = nums[l];
+            var maxIndex = l;
+            for (int i = l +1; i <= r; i++)
+            {
+                if (max < nums[i])
+                {
+                    maxIndex = i;
+                    max = nums[i];
+                }
+            }
+            var note = new TreeNode(max);
+            note.left = Helper(nums, l, maxIndex - 1);
+            note.right = Helper(nums, maxIndex + 1, r);
+            return note;
+        }
+
+        /*  O（n） solution
+        public TreeNode constructMaximumBinaryTree(int[] nums)
+        {
+            LinkedList<TreeNode> lklist = new LinkedList<>();
+            TreeNode left = null;
+            for (int num: nums)
+            {
+                TreeNode cur = new TreeNode(num);
+                while (!lklist.isEmpty() && lklist.peekFirst().val < cur.val)
+                {
+                    cur.left = lklist.pop();
+                }
+
+                if (!lklist.isEmpty())
+                {
+                    lklist.peekFirst().right = cur;
+                }
+                lklist.push(cur);
+            }
+
+            return lklist.peekLast();
+        }
+        */
+    }
+}
