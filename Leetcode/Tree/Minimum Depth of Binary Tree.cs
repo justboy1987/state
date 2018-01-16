1b922388a6ae2376d85fd48ed313579cf54703e8
justboy1987 <175636422@qq.com>
2018-01-16 12:29:51 +0800

    Minimum_Depth_of_Binary_Tree

---
+++ Leetcode/Tree/Minimum Depth of Binary Tree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Tree
+{
+    class Minimum_Depth_of_Binary_Tree
+    {
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+            if (root.left == null)
+                return MinDepth(root.right) + 1;
+            if (root.right == null)
+                return MinDepth(root.left) + 1;
+            return Math.Min(MinDepth(root.right), MinDepth(root.left)) + 1;
+        }
+    }
+}
---
+++ Leetcode/Tree/Path Sum II.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Tree
+{
+    class Path_Sum_II
+    {
+        IList<IList<int>> ret = new List<IList<int>>();
+
+        public IList<IList<int>> PathSum(TreeNode root, int sum)
+        {
+            Helper(root, sum, new List<int>());
+            return ret;
+        }
+        private void Helper(TreeNode root, int sum, IList<int> pathes)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            pathes.Add(root.val);
+            if (root.left == null && root.right == null)
+            {
+                if (sum == root.val)
+                {
+                    ret.Add(new List<int>(pathes));
+                }
+            }
+            Helper(root.left, sum - root.val, pathes);
+            Helper(root.right, sum - root.val, pathes);
+            pathes.RemoveAt(pathes.Count - 1);
+        }
+    }
+}
