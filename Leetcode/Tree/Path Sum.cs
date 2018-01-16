32829c6e168d41892112ac231c8ec15d36c67c08
justboy1987 <175636422@qq.com>
2018-01-16 12:16:33 +0800

    Path_Sum

---
+++ Leetcode/Tree/Path Sum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Tree
+{
+    class Path_Sum
+    {
+        private bool hasPath = false;
+        public bool HasPathSum(TreeNode root, int sum)
+        {
+            Helper(root, sum);
+            return hasPath;
+        }
+
+        private void Helper(TreeNode root, int sum)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            if (root.left == null && root.right == null)
+            {
+                if (sum == root.val)
+                {
+                    hasPath = true;
+                }
+            }
+            Helper(root.left, sum - root.val);
+            Helper(root.right, sum - root.val);
+        }
+    }
+}
