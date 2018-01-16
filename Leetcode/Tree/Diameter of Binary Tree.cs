2b2551da313d87c6c1b82dbef3560a2d3f052c61
justboy1987 <175636422@qq.com>
2018-01-16 08:39:38 +0800

    DiameterOfBinaryTree
    DiameterOfBinaryTree

---
+++ Leetcode/Tree/Diameter of Binary Tree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Tree
+{
+    class DiameterOfBinaryTreeSLN
+    {
+        int max = 0;
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            MaxDepth(root);
+            return max;
+        }
+
+
+        private int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            int left = MaxDepth(root.left);
+            int right = MaxDepth(root.right);
+
+            max = Math.Max(max, left + right);
+
+            return Math.Max(left, right) + 1;
+        }
+    }
+}
