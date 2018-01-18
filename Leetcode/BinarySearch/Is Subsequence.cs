b5447e2afd2297e001dcd8378e75b6a6cfd60a71
justboy1987 <175636422@qq.com>
2018-01-18 13:19:06 +0800

    kth_smallest_number_in_multiplication_table

---
+++ Leetcode/BinarySearch/Is Subsequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.BinarySearch
+{
+    class Is_Subsequence
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            int j = 0;
+            for (int i = 0; i < t.Length && j < s.Length; i++)
+            {
+                if (s[j] == t[i])
+                {
+                    j++;
+                }
+            }
+            return j == s.Length - 1;
+        }
+    }
+}
---
+++ Leetcode/BinarySearch/kth-smallest-number-in-multiplication-table.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.BinarySearch
+{
+    class kth_smallest_number_in_multiplication_table
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="n"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int FindKthNumber(int m, int n, int k)
+        {
+            int low = 1, high = m * n;
+
+            while (low < high)
+            {
+                int mid = low + (high - low) / 2;
+                int c = Count(mid, m, n);
+                if (c >= k) high = mid; //因为可以是 mid 所以不减1
+                else low = mid + 1;
+                
+            }
+
+            return high;
+        }
+
+        private int Count(int v, int m, int n)
+        {
+            int count = 0;
+            for (int i = 1; i <= m; i++)
+            {
+                int temp = Math.Min(v / i, n);
+                count += temp;
+            }
+            return count;
+        }
+    }
+}
