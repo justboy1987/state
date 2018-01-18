af35456d94742e3f37d390ab8d4b011c07be9eab
justboy1987 <175636422@qq.com>
2018-01-18 13:25:16 +0800

    Two_Sum_II___Input_array_is_sorted

---
+++ Leetcode/BinarySearch/Two Sum II - Input array is sorted.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.BinarySearch
+{
+    class Two_Sum_II___Input_array_is_sorted
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            for (int i = 0, j = numbers.Length - 1; i < j;)
+            {
+                if (numbers[i] + numbers[j] == target)
+                {
+                    return new[] {i + 1, j + 1};
+                }else if (numbers[i] + numbers[j] < target)
+                {
+                    i++;
+                }
+                else
+                {
+                    j--;
+                }
+            }
+            return null;
+        }
+    }
+}
