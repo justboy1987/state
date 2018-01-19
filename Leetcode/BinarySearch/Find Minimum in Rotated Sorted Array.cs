0d80ca7009d95ca53d008bb41835d4b854fa8bc9
justboy1987 <175636422@qq.com>
2018-01-19 14:55:09 +0800

    FindMin

---
+++ Leetcode/BinarySearch/Find Minimum in Rotated Sorted Array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.BinarySearch
+{
+    class Find_Minimum_in_Rotated_Sorted_Array
+    {
+        public int FindMin(int[] nums)
+        {
+            int lo = 0, hi = nums.Length - 1;
+            while (lo < hi)
+            {
+                int mid = (lo + hi) / 2;
+                if (nums[mid] > nums[hi]) lo = mid + 1;
+                else hi = mid;
+            }
+            return nums[lo];
+        }
+        
+    }
+}
