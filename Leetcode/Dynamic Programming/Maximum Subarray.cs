13f0e43ac90a3e0a9c33e11aa9c791d1cd31c223
justboy1987 <175636422@qq.com>
2018-01-24 14:22:10 +0800

    Maximum_Subarray

---
+++ Leetcode/Dynamic Programming/Maximum Subarray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Dynamic_Programming
+{
+    /// <summary>
+    /// https://leetcode.com/problems/maximum-subarray/description/
+    /// </summary>
+    class Maximum_Subarray
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            var max = nums[0];
+            var globalMax = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                max = Math.Max(nums[i], nums[i] + max);
+                if (max > globalMax) globalMax = max;
+            }
+            return globalMax;
+        }
+    }
+}
