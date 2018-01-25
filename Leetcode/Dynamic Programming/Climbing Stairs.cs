8543acd4e65b0a6f2f7c15b75b419f11f1990d1e
justboy1987 <175636422@qq.com>
2018-01-25 12:45:13 +0800

    Climbing_Stairs

---
+++ Leetcode/Dynamic Programming/Climbing Stairs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Dynamic_Programming
+{
+    /// <summary>
+    /// https://leetcode.com/problems/climbing-stairs/description/
+    /// https://leetcode.com/problems/climbing-stairs/solution/
+    /// </summary>
+    class Climbing_Stairs
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n == 0 || n == 1)
+                return 1;
+            int f0 = 1, f1 = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                var t = f0 + f1;
+                f0 = f1;
+                f1 = t;
+            }
+            return f1;
+        }
+
+        public int climbStairs(int n)
+        {
+            double sqrt5 = Math.Sqrt(5);
+            double fibn = Math.Pow((1 + sqrt5) / 2, n + 1) - Math.Pow((1 - sqrt5) / 2, n + 1);
+            return (int)(fibn / sqrt5);
+        }
+    }
+}
---
+++ Leetcode/Dynamic Programming/House Robber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Dynamic_Programming
+{
+    /// <summary>
+    /// https://leetcode.com/problems/house-robber/description/
+    /// </summary>
+    class House_Robber
+    {
+        public int Rob(int[] nums)
+        {
+            int prevNo = 0;
+            int prevYes = 0;
+            foreach (var n in nums)
+            {
+                int temp = prevNo;
+                prevNo = Math.Max(prevNo, prevYes);
+                prevYes = n + temp;
+            }
+            return Math.Max(prevNo, prevYes);
+        }
+    }
+}
