86784889f5101d972aed2fcbf5deca30596840b1
justboy1987 <175636422@qq.com>
2018-02-04 13:46:05 +0800

    _3Sum_Closest

---
+++ Leetcode/Array/3Sum Closest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Array
+{
+    class _3Sum_Closest
+    {
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            var result = 0;
+            System.Array.Sort(nums);
+            int min = int.MaxValue;
+
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int j = i + 1;
+                int k = nums.Length - 1;
+                while (j < k)
+                {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    int diff = Math.Abs(sum - target);
+
+                    if (diff == 0) return sum;
+
+                    if (diff < min)
+                    {
+                        min = diff; // update min
+                        result = sum;  // update result
+                    }
+                    if (sum <= target)
+                    {
+                        j++;
+                    }
+                    else
+                    {
+                        k--;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
