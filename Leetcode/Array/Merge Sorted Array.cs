d9d3a49b825d54d515c0559e7058841add71d2e2
justboy1987 <175636422@qq.com>
2018-02-07 05:25:11 +0800

    Merge_Sorted_Array

---
+++ Leetcode/Array/Merge Sorted Array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Array
+{
+    /// <summary>
+    /// https://leetcode.com/problems/merge-sorted-array/description/
+    /// </summary>
+    class Merge_Sorted_Array
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int i = m - 1, j = n - 1, k = m + n - 1;
+            while (i > -1 && j > -1) nums1[k--] = (nums1[i] > nums2[j]) ? nums1[i--] : nums2[j--];
+            while (j > -1) nums1[k--] = nums2[j--];
+        }
+    }
+}
