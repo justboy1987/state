57f47c49370175385767035ef8d647555eebe9a6
justboy1987 <175636422@qq.com>
2018-04-02 06:29:49 +0800

    Max_Area_of_Island
    Max_Area_of_Island

---
+++ Leetcode/Array/Max Area of Island.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Array
+{
+    /// <summary>
+    /// https://leetcode.com/problems/max-area-of-island/description/
+    /// </summary>
+    class Max_Area_of_Island
+    {
+        public int MaxAreaOfIsland(int[,] grid)
+        {
+            var result = new int[grid.GetLength(0), grid.GetLength(1)];
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    if (grid[i, j] == 1)
+                    {
+                        if (i == 0 && j == 0)
+                        {
+                            result[i, j] = 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
